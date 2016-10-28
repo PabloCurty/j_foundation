31c64c5cccba8323ab3e763acb5f16f6d984c874
Rafael.starke <rafael.starke@govbr.com.br>
2016-10-28 15:59:39 -0200

    Conversão do Projeto para Maven

--- src/main/java/br/com/java_foundation/webservice/WS_inicia_servicos.java
+++ src/main/java/br/com/java_foundation/webservice/WS_inicia_servicos.java
@@ -2,17 +2,17 @@ package br.com.java_foundation.webservice;
 
 import java.util.HashSet;
 import java.util.Set;
- 
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
  
 @ApplicationPath("/rs")
-public class WS_inicia_servico extends Application {
+public class WS_inicia_servicos extends Application {
  
 	private Set<Object> singletons = new HashSet<Object>();
 	private Set<Class<?>> empty = new HashSet<Class<?>>();
  
-	public WS_inicia_servico() {
+	public WS_inicia_servicos() {
 		singletons.add(new StatusService());
 		singletons.add(new EntidadeServiceImpl());
 	}
